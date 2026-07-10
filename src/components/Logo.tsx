c8398f1f2f92120b739a1cfade8d762d6f85810b
t000909 <tan@10panda.com>
2026-07-10 12:18:47 +0700

    Logo settled

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logoUrl from '../assets/logo.png';
 
 export default function Logo({ className = "" }: { className?: string }) {
   return (
     <div className={`flex items-center select-none ${className}`}>
       <img
-        src="/logo.png"
+        src={logoUrl}
         alt="Team Building Logo"
         className="h-8 md:h-10 w-auto object-contain"
         referrerPolicy="no-referrer"
